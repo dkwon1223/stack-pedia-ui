dc568b1b25b747019abadbba0f449e41b2319178
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-04-15 16:25:49 -0600

    Add basic styling and structure to Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import Nav from './Nav/Nav';
+import Hero from './Hero/Hero';
+import Information from './Information/Information';
+import Footer from './Footer/Footer';
+
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <Information />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+)
+
+export default App;
---
+++ src/Nav/Nav.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import StackIcon from '../assets/icons/stack-icon.svg';
+import hamburger from '../assets/icons/hamburger.svg';
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+        <nav className='flex justify-between items-center max-container'>
+            <a href="/" className='flex items-center'>
+                <img 
+                    src={StackIcon}
+                    alt="Logo"
+                    width={40}
+                    height={10}
+                />
+                <h1 className='ml-2 text-2xl font-bold text-orange-500'>StackPedia</h1>
+            </a>
+            <ul className='flex-1 flex justify-end items-center gap-12 max-lg:hidden mr-10'>
+                <li className='font-monserrat leading-normal text-lg text-slate-gray cursor-pointer hover:underline'>
+                    <a>Technologies</a>
+                </li>
+                <li className='font-monserrat leading-normal text-lg text-slate-gray cursor-pointer hover:underline '>
+                    <a>Stacks</a>
+                </li>
+            </ul>
+            <div className='hidden max-lg:block'>
+                <img
+                    src={hamburger}
+                    alt="Hamburger dropdown"
+                    width={25}
+                    height={25}
+                />
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
