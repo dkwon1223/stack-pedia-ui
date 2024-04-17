b6661d0f6d158b92a98b89f63c11ca23ca157539
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-04-16 20:09:45 -0600

    Add useState hooks for filter state

--- src/TechFilterBar/FilterBar.jsx
+++ src/TechFilterBar/FilterBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ArrowDown from "../assets/icons/arrow-down.svg";
 
-const TechFilterBar = ({ technologies, fetchTechnologies }) => {
+const TechFilterBar = ({ technologies, fetchFilteredTechnologies, setIsFiltered}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const overallTypes = technologies.reduce((acc, tech) => {
@@ -12,14 +12,15 @@ const TechFilterBar = ({ technologies, fetchTechnologies }) => {
   }, []);
 
   async function filterTechnologies(category) {
-    await fetchTechnologies(category)
+    setIsFiltered(true);
+    await fetchFilteredTechnologies(category);
   }
 
   return (
     <>
       <button
         onClick={() => setIsOpen((prev) => !prev)}
-        className="w-1/5 flex justify-evenly items-center mt-24 bg-coral-red h-12 rounded-lg border-4 border-transparent active:border-white duration-600 active:text-white font-bold tracking-wider font-palanquin"
+        className="w-1/5 flex justify-evenly items-center mt-24 bg-orange-400 h-12 rounded-lg border-4 border-transparent active:border-white duration-600 active:text-white font-bold tracking-wider font-palanquin"
       >
         Filter by Category
         {!isOpen ? (
@@ -29,14 +30,17 @@ const TechFilterBar = ({ technologies, fetchTechnologies }) => {
         )}
       </button>
       {isOpen && (
-        <div className="bg-coral-red flex flex-col justify-center items-center w-1/5 mt-2 rounded-lg">
-          {overallTypes.map((type, index) => {
-            return (
-              <button key={index} onClick={() => filterTechnologies(type)} className="bg-white w-1/2 rounded-sm my-2 font-semibold font-palanquin">
-                {type}
-              </button>
-            );
-          })}
+        <div className="bg-orange-400 flex flex-col justify-center items-center w-1/5 mt-2 rounded-lg">
+          <>
+            {overallTypes.map((type, index) => {
+              return (
+                <button key={index} onClick={() => filterTechnologies(type)} className="bg-white w-1/2 rounded-sm my-2 font-semibold font-palanquin">
+                  {type}
+                </button>
+              );
+            })}
+            <button onClick={() => setIsFiltered(false)} className="bg-white w-1/2 rounded-sm my-2 font-semibold font-palanquin">all</button>
+          </>
         </div>
       )}
     </>
--- src/Technologies/Technologies.jsx
+++ src/Technologies/Technologies.jsx
@@ -5,38 +5,81 @@ import TechFilterBar from "../TechFilterBar/FilterBar";
 
 const Technologies = () => {
   const [technologies, setTechnologies] = useState([]);
+  const [filteredTechnologies, setFilteredTechnologies] = useState([]);
+  const [isFiltered, setIsFiltered] = useState(false);
+
   const fetchTechnologies = async (endpoint) => {
     try {
-        const response = await fetch(`http://localhost:8080/api/v1/technologies/${endpoint}`)
-        if(!response.ok) {
-            throw new Error("Failed to retrieve data. Try again later.");
-        }
-        const data = await response.json();
-        setTechnologies(data);
+      const response = await fetch(
+        `http://localhost:8080/api/v1/technologies/${endpoint}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to retrieve data. Try again later.");
+      }
+      const data = await response.json();
+      setTechnologies(data);
     } catch (error) {
-        console.log(Error, error);
+      console.log(Error, error);
     }
-  }
+  };
+
+  const fetchFilteredTechnologies = async (endpoint) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/api/v1/technologies/${endpoint}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to retrieve data. Try again later.");
+      }
+      const data = await response.json();
+      setFilteredTechnologies(data);
+    } catch (error) {
+      console.log(Error, error);
+    }
+  };
+
   useEffect(() => {
     fetchTechnologies("all");
-  }, [])
+  }, []);
 
   return (
     <>
       <Nav />
       <section className="flex flex-col justify-center items-center">
-        <TechFilterBar technologies={technologies} setTechnologies={setTechnologies} fetchTechnologies={fetchTechnologies}/>
+        <TechFilterBar
+          technologies={technologies}
+          setTechnologies={setTechnologies}
+          fetchTechnologies={fetchTechnologies}
+          fetchFilteredTechnologies={fetchFilteredTechnologies}
+          isFiltered={isFiltered}
+          setIsFiltered={setIsFiltered}
+        />
         <section className="grid grid-cols-6 grid-rows-[auto] auto-rows-fr pt-12 w-full gap-x-5 gap-y-5 px-8">
-          {technologies.map((tech) => {
+          {isFiltered ? (filteredTechnologies.map((tech) => {
             return (
-              <div key={tech.name} className="flex flex-col justify-evenly items-center max-h-44 border rounded-lg px-2 hover:hover:bg-orange-300 hover:scale-110 cursor-pointer hover:shadow-xl">
-                <img src={tech.image_url} className="h-1/3"/>
+              <div
+                key={tech.name}
+                className="flex flex-col justify-evenly items-center max-h-44 border rounded-lg px-2 hover:hover:bg-orange-300 hover:scale-110 cursor-pointer hover:shadow-xl"
+              >
+                <img src={tech.image_url} className="h-1/3" />
                 <h1 className="font-bold text-center">{tech.name}</h1>
               </div>
-            )
-          })}
+            );
+          })) 
+          : 
+          (technologies.map((tech) => {
+            return (
+              <div
+                key={tech.name}
+                className="flex flex-col justify-evenly items-center max-h-44 border rounded-lg px-2 hover:hover:bg-orange-300 hover:scale-110 cursor-pointer hover:shadow-xl"
+              >
+                <img src={tech.image_url} className="h-1/3" />
+                <h1 className="font-bold text-center">{tech.name}</h1>
+              </div>
+            );
+          }))}
         </section>
-    </section>
+      </section>
     </>
   );
 };
