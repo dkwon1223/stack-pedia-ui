33750adf59c7ebc664872a81808ae6d7d072e624
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-05-11 01:51:42 -0600

    Add nav to login and signup component views

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Nav from "./Nav";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -10,22 +11,25 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h3>Log In</h3>
-      <label>Email:</label>
-      <input
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <label>Password:</label>
-      <input
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <button>Log In</button>
-    </form>
+    <section>
+      <Nav />
+      <form onSubmit={handleSubmit}>
+        <h3>Log In</h3>
+        <label>Email:</label>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <button>Log In</button>
+      </form>
+    </section>
   );
 };
 
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Nav from "./Nav";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
@@ -10,22 +11,25 @@ const Signup = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h3>Sign Up</h3>
-      <label>Email:</label>
-      <input
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <label>Password:</label>
-      <input
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <button>Sign Up</button>
-    </form>
+    <section>
+      <Nav />
+      <form onSubmit={handleSubmit}>
+        <h3>Sign Up</h3>
+        <label>Email:</label>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <button>Sign Up</button>
+      </form>
+    </section>
   );
 };
 
