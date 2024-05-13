8ca5a35e24606db1b8f9bbad72eefea3e472ab0f
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-05-12 22:00:44 -0600

    Add UI feedback indicating if technology is already favorited

--- src/Components/DetailedTechCard.jsx
+++ src/Components/DetailedTechCard.jsx
@@ -7,20 +7,62 @@ import PropTypes from "prop-types";
 import { useFavoriteTech } from "../Hooks/useFavoriteTech";
 import { useAuthContext } from "../Hooks/useAuthContext";
 import { jwtDecode } from "jwt-decode";
+import { useState, useEffect } from "react";
 
 const DetailedTechCard = ({ technology }) => {
-  const  { favoriteTech } = useFavoriteTech();
+  const { favoriteTech } = useFavoriteTech();
   const { user } = useAuthContext();
-  
+  const [favorite, setFavorite] = useState(false);
+  const userLocal = JSON.parse(localStorage.getItem("user"));
+
+  const fetchUserFav = async (endpoint) => {
+    try {
+      const response = await fetch(
+        `https://stack-pedia-api.adaptable.app/api/v1/user/${endpoint}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to retrieve data. Try again later.");
+      }
+      const data = await response.json();
+      const techList = await data.favoriteTechs;
+      const userFavoriteIds = await techList.map((tech) => {
+        return tech._id;
+      });
+      console.log(userFavoriteIds);
+      console.log(technology._id);
+      const isFavorite = await userFavoriteIds.includes(technology._id);
+      console.log(isFavorite);
+      setFavorite(isFavorite);
+    } catch (error) {
+      console.log(Error, error);
+    }
+  };
+
   function handleFavorite() {
-    if(user.token) {
-      const decode = jwtDecode(user.token)
-      const loggedInId = decode._id
+    if (user.token) {
+      const decode = jwtDecode(userLocal.token);
+      const loggedInId = decode._id;
       favoriteTech(technology, loggedInId);
+      setFavorite(true);
     } else {
-      favoriteTech(technology, user._id);
+      favoriteTech(technology, userLocal._id);
+      setFavorite(true);
     }
   }
+
+  const handleUser = () => {
+    if (userLocal.token) {
+      const decode = jwtDecode(userLocal.token);
+      const loggedInId = decode._id;
+      fetchUserFav(loggedInId);
+    } else {
+      fetchUserFav(userLocal._id);
+    }
+  };
+
+  useEffect(() => {
+    handleUser();
+  }, [favorite]);
 
   return (
     <section>
@@ -28,7 +70,10 @@ const DetailedTechCard = ({ technology }) => {
         id="techCard"
         className="w-screen flex xl:flex-row flex-col justify-center gap-10 max-container lg:mt-52 xsm:mt-80 md:mt-96"
       >
-        <figure id="techCardImage" className="flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-4 bg-primary bg-hero bg-cover bg-center bg-gradient-to-b-0.2 md:mt-36 lg:mt-0 xsm:mt-12">
+        <figure
+          id="techCardImage"
+          className="flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-4 bg-primary bg-hero bg-cover bg-center bg-gradient-to-b-0.2 md:mt-36 lg:mt-0 xsm:mt-12"
+        >
           <img
             src={technology.image2_url}
             alt={`cover image showing {${technology.name}}`}
@@ -37,7 +82,10 @@ const DetailedTechCard = ({ technology }) => {
             className="object-contain relative z-10"
           />
         </figure>
-        <article id="techCardText" className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x ml-10 xsm:ml-2">
+        <article
+          id="techCardText"
+          className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x ml-10 xsm:ml-2"
+        >
           <h1 className="font-palanquin text-4xl max-sm:text-[36px] max-sm:leading-[41px] font-bold mb-2">
             {technology.name}
             <br />
@@ -73,12 +121,22 @@ const DetailedTechCard = ({ technology }) => {
               </ul>
             </div>
           </article>
-          <a href={technology.documentation_link} target="_blank" className="mb-4">
+          <a
+            href={technology.documentation_link}
+            target="_blank"
+            className="mb-4"
+          >
             <Button label="Visit Documentation" iconUrl={DocIcon} />
           </a>
-          {user && (<div onClick={handleFavorite}>
-            <Button label="Favorite this" iconUrl={FavIcon}/>
-          </div>)}
+          {user && !favorite ? (
+            <div onClick={handleFavorite}>
+              <Button label="Favorite this" iconUrl={FavIcon} />
+            </div>
+          ) : (
+            <div onClick={handleFavorite}>
+              <Button label="Already favorited" iconUrl={FavIcon} disabled={true} color="orange-700"/>
+            </div>
+          )}
         </article>
       </section>
       <Footer />
@@ -89,5 +147,5 @@ const DetailedTechCard = ({ technology }) => {
 export default DetailedTechCard;
 
 DetailedTechCard.propTypes = {
-  technology: PropTypes.object.isRequired
-}
+  technology: PropTypes.object.isRequired,
+};
